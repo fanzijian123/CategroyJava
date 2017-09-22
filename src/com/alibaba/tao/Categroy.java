24d87c2f6a139cd5f5d592ae40ebe6171e9dc6fa
fanjian <fj@DESKTOP-57J4G5J.lan>
2017-09-22 19:14:32 +0800

    1.提交一个接口

--- src/com/alibaba/tao/Categroy.java
+++ src/com/alibaba/tao/Categroy.java
@@ -1,5 +1,7 @@
 package com.alibaba.tao;
 
 public class Categroy {
-
+	public void printHello(){
+		System.out.println("你好，Github");
+	}
 }
---
+++ src/com/alibaba/tao/ICategroy.java
@@ -0,0 +1,5 @@
+package com.alibaba.tao;
+
+public interface ICategroy {
+
+}
