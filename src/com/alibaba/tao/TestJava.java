f51f27aed0342fda2cc77767fa7cc9a8a2cb2b66
fanjian <fj@DESKTOP-57J4G5J.lan>
2017-09-22 18:40:21 +0800

    1.项目初次提交

---
+++ src/com/alibaba/tao/TestJava.java
@@ -0,0 +1,13 @@
+package com.alibaba.tao;
+
+/**
+ * 
+ * @author fj
+ *
+ */
+public class TestJava {
+	public void printHello(){
+		System.out.println("你好，Github");
+	}
+
+}
---
+++ src/com/alibaba/tao/TestJavaRun.java
@@ -0,0 +1,12 @@
+package com.alibaba.tao;
+
+public class TestJavaRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestJava testJava = new TestJava();
+		testJava.printHello();
+
+	}
+
+}
